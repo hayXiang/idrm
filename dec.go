6c66d3f9e92600c55d855c79507b5eee60d5d9cb
haycker <86708160+hayXiang@users.noreply.github.com>
2025-08-28 10:44:29 +0800

    Update dec.go

--- dec.go
+++ dec.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"crypto/aes"
 	"crypto/cipher"
 	"encoding/hex"
@@ -10,26 +11,86 @@ import (
 	"github.com/Eyevinn/mp4ff/mp4"
 )
 
-// AES-CENC解密
-func decryptCENC(data, key, iv []byte) ([]byte, error) {
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return nil, err
-	}
-	stream := cipher.NewCTR(block, iv)
-	decrypted := make([]byte, len(data))
-	stream.XORKeyStream(decrypted, data)
-	return decrypted, nil
-}
-
 // 补齐 IV 到 16 字节（前补0）
 func padIV(iv8 []byte) []byte {
 	iv := make([]byte, 16)
-	copy(iv[8:], iv8)
+	copy(iv, iv8)
 	return iv
 }
 
-func decryptWidevine(inPath, outPath string, key []byte) error {
+func decryptWidevine(mp4File *mp4.File, key []byte) error {
+	var senc *mp4.SencBox
+	// 查找 senc box并获取IV
+	trak := mp4File.LastSegment().LastFragment().Moof.Traf
+	var newBoxes []mp4.Box
+	for _, box := range trak.Children {
+		if box.Type() == "senc" {
+			senc, _ = box.(*mp4.SencBox)
+		} else if (box.Type() != "saio") && (box.Type() != "saiz") {
+			newBoxes = append(newBoxes, box)
+		}
+	}
+	trak.Children = newBoxes
+
+	if senc == nil {
+		return fmt.Errorf("未找到 senc box 或 IV")
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return err
+	}
+	// 解密 mdat
+	var mdata = mp4File.LastSegment().LastFragment().Mdat
+	if mdata != nil {
+		encrypted := mdata.Data
+		var decrypted []byte
+		var offset uint32 = 0
+		for i := 0; i < len(senc.IVs); i++ {
+			iv := padIV(senc.IVs[i])
+			stream := cipher.NewCTR(block, iv)
+
+			if senc.SubSamples == nil {
+				sampleSize := trak.Trun.Samples[i].Size
+				part := make([]byte, sampleSize)
+				stream.XORKeyStream(part, encrypted[offset:offset+sampleSize])
+				decrypted = append(decrypted, part...)
+				offset += sampleSize
+			} else {
+				sample := senc.SubSamples[i]
+				for _, sub := range sample {
+					clearLen := (uint32)(sub.BytesOfClearData)
+					cipherLen := (uint32)(sub.BytesOfProtectedData)
+
+					if clearLen > 0 {
+						decrypted = append(decrypted, encrypted[offset:offset+clearLen]...)
+						offset += clearLen
+					}
+
+					if cipherLen > 0 {
+						part := make([]byte, cipherLen)
+						stream.XORKeyStream(part, encrypted[offset:offset+cipherLen])
+						decrypted = append(decrypted, part...)
+						offset += cipherLen
+					}
+				}
+			}
+		}
+		mdata.Data = decrypted
+	}
+	return nil
+}
+
+func decryptWidevineFromBody(data []byte, key []byte) ([]byte, error) {
+	mp4File, err := mp4.DecodeFile(bytes.NewReader(data))
+	if err != nil {
+		return nil, fmt.Errorf("解析 MP4 文件失败: %w", err)
+	}
+	decryptWidevine(mp4File, key)
+	return encodeMP4ToBytes(mp4File)
+}
+
+func decryptWidevineFromFile(inPath, outPath string, key []byte) error {
 	inFile, err := os.Open(inPath)
 	if err != nil {
 		return fmt.Errorf("打开输入文件失败: %w", err)
@@ -41,54 +102,7 @@ func decryptWidevine(inPath, outPath string, key []byte) error {
 		return fmt.Errorf("解析 MP4 文件失败: %w", err)
 	}
 
-	var senc *mp4.SencBox
-	// 查找 senc box并获取IV
-	for _, trak := range mp4File.LastSegment().LastFragment().Moof.Trafs {
-		var newBoxes []mp4.Box
-		for _, box := range trak.Children {
-			if box.Type() == "senc" {
-				senc, _ = box.(*mp4.SencBox)
-			} else {
-				newBoxes = append(newBoxes, box)
-			}
-		}
-		trak.Children = newBoxes
-	}
-
-	if senc == nil {
-		return fmt.Errorf("未找到 senc box 或 IV")
-	}
-
-	// 解密 mdat
-	var mdata = mp4File.LastSegment().LastFragment().Mdat
-	if mdata != nil {
-		encrypted := mdata.Data
-		decrypted := make([]byte, 0, len(encrypted))
-		offset := 0
-		for i, sample := range senc.SubSamples {
-			iv := padIV(senc.IVs[i])
-			for _, sub := range sample {
-				clearLen := int(sub.BytesOfClearData)
-				cipherLen := int(sub.BytesOfProtectedData)
-				// 先拼接明文
-				if clearLen > 0 {
-					decrypted = append(decrypted, encrypted[offset:offset+clearLen]...)
-					offset += clearLen
-				}
-				// 再拼接解密后的密文
-				if cipherLen > 0 {
-					part, err := decryptCENC(encrypted[offset:offset+cipherLen], key, iv)
-					if err != nil {
-						return fmt.Errorf("解密失败: %w", err)
-					}
-					decrypted = append(decrypted, part...)
-					offset += cipherLen
-				}
-			}
-		}
-		mdata.Data = decrypted
-	}
-
+	decryptWidevine(mp4File, key)
 	outFile, err := os.Create(outPath)
 	if err != nil {
 		return fmt.Errorf("创建输出文件失败: %w", err)
@@ -102,7 +116,7 @@ func decryptWidevine(inPath, outPath string, key []byte) error {
 	return nil
 }
 
-func main() {
+func zmain() {
 	if len(os.Args) != 4 {
 		fmt.Println("用法: dec <输入文件> <输出文件> <密钥(hex)>")
 		return
@@ -117,7 +131,7 @@ func main() {
 		return
 	}
 
-	err = decryptWidevine(inPath, outPath, key)
+	err = decryptWidevineFromFile(inPath, outPath, key)
 	if err != nil {
 		fmt.Println("处理失败:", err)
 	} else {
