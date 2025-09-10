c8faa76a80a0ac16a07fca42665a1f14b0993f4f
haycker <86708160+hayXiang@users.noreply.github.com>
2025-09-10 15:01:03 +0000

    code review

--- proxy.go
+++ proxy.go
@@ -876,8 +876,8 @@ func DashToHLS(mpdUrl string, body []byte, tvgId string) (map[string]string, err
 						groupID, lang, lang, tvgId, playlistName))
 			} else {
 				masterLines = append(masterLines,
-					fmt.Sprintf(`#EXT-X-STREAM-INF:BANDWIDTH=%s,RESOLUTION=%s,CODECS="%s",AUDIO="%s"`,
-						bandwidth, resolution, codecs, groupID))
+					fmt.Sprintf(`#EXT-X-STREAM-INF:BANDWIDTH=%s,RESOLUTION=%s,CODECS="%s",AUDIO="audio"`,
+						bandwidth, resolution, codecs))
 				masterLines = append(masterLines, fmt.Sprintf("/drm/proxy/hls/%s/%s", tvgId, playlistName))
 			}
 
