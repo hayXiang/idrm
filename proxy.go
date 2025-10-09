d8be91b508733a085cd0120f5304d1424267a9fe
haycker <86708160+hayXiang@users.noreply.github.com>
2025-10-09 06:32:34 +0000

    1.0.0.24

--- proxy.go
+++ proxy.go
@@ -85,7 +85,7 @@ var (
 	CACHE_302_REDIRECT_URL     = cache.New(60*time.Second, 30*time.Second)
 )
 
-var version = "1.0.0.23"
+var version = "1.0.0.24"
 
 func loadConfigFile(path string) ([]StreamConfig, error) {
 	f, err := os.ReadFile(path)
