5aa490061da6a7d7ce84448197cf8c70ce1e1bd5
haycker <86708160+hayXiang@users.noreply.github.com>
2025-10-12 01:09:58 +0000

    code review.

--- hls.go
+++ hls.go
@@ -289,7 +289,7 @@ func DashToHLS(mpdUrl string, body []byte, tvgId string, bestQuality bool) (stri
 					groupID, lang, lang, tvgId, playlistName))
 			default:
 				line := fmt.Sprintf(`#EXT-X-STREAM-INF:BANDWIDTH=%s,RESOLUTION=%s,CODECS="%s",AUDIO="audio"`,
-					bandwidth, resolution, strings.ReplaceAll("hev1", "hvc1", codecs))
+					bandwidth, resolution, codecs)
 				if subtitleExists {
 					line += `,SUBTITLES="subs"`
 				}
