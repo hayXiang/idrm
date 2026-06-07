41eeb57a5674659e123913a30382b10c2a13f1ba
xianghai <xianghai@example.com>
2026-06-07 11:43:55 +0800

    1.0.0.42

--- proxy.go
+++ proxy.go
@@ -71,7 +71,7 @@ var (
 	VISIT_TRACKER              = NewVisitTracker()
 )
 
-var Version = "1.0.0.41"
+var Version = "1.0.0.42"
 
 // newHTTPClient 创建支持 SOCKS5 或 HTTP 代理的 net/http Client
 func newHTTPClient(proxyURL string, timeout int) *http.Client {
